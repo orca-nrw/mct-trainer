47a70591a8f8b2fa84b8a081efc91f4f781c7872
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-23 13:36:48 +0200

    Add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import TrainerRouter from './Components/TrainerRouter'
+import { Footer } from './Components/UI/Footer'
+import { Header } from './Components/UI/Header'
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="max-w-screen-xl space-y-4 mx-auto my-0 flex flex-col justify-center">
+      <Header />
+      <TrainerRouter />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
