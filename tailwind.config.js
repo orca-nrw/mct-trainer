9019fade8f8c69bd7469ad87421a91a9cd7068e8
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-10-23 13:39:36 +0200

    Fixed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       colors: {
         'th-red': '#c81e0f',
         'th-orange': '#ea5a00',
-        'th-violet': '#b43092'
-      }
+        'th-violet': '#b43092',
+      },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 }
