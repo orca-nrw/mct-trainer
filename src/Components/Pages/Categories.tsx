57e48e51f49052e27387004703659bf0ede68375
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-02 18:28:23 +0100

    Created

---
+++ src/Components/Pages/Categories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TrainerHeader from '../UI/TrainerHeader'
+
+export default function Categories() {
+  return (
+    <div className="space-y-4">
+      <TrainerHeader>Kategorien</TrainerHeader>
+      <p>Wählen Sie die gewünschten Kategorien aus:</p>
+    </div>
+  )
+}
