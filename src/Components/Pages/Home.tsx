fc58dae4464d31b4e949a672c764bb1cf51dc512
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-02 18:04:32 +0100

    First draft

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../UI/Button'
 
 export default function Home() {
-  return <div>Hello World!</div>
+  return (
+    <div className="flex flex-col items-center space-y-8 p-4">
+      <h1 className="text-4xl font-bold">Willkommen!</h1>
+      <p className="text-lg text-center">
+        Der Multiple-Choice-Test (MCT) enthält mehr als 1300 Fragen zum Thema
+        Datenbanken, aus denen 15 Fragen zufällig ausgewählt und bewertet
+        werden.
+      </p>
+
+      <Link to="/categories">
+        <Button>
+          Weiter
+        </Button>
+      </Link>
+    </div>
+  )
 }
