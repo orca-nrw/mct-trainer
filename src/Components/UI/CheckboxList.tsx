252a9bdd4afdf6b285542b3417afe26e85aaee96
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-02 22:19:20 +0100

    Fixed map

--- src/Components/UI/CheckboxList.tsx
+++ src/Components/UI/CheckboxList.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 interface PropTypes {
   entries: string[]
   checked: boolean[]
-  setChecked: (newState: Boolean[]) => void
+  setChecked: (newState: boolean[]) => void
 }
 
 export default function CheckboxList({
@@ -11,7 +11,6 @@ export default function CheckboxList({
   checked,
   setChecked,
 }: PropTypes) {
-
   const handleOnChange = (position: number) => {
     const updatedCheckedState = checked.map((item, index) =>
       index === position ? !item : item
@@ -23,21 +22,23 @@ export default function CheckboxList({
   return (
     <ul>
       {entries.map((value, index) => {
-        ;<li key={index}>
-          <div>
-            <input
-              type="checkbox"
-              name={value}
-              id={`checkbox-${index}`}
-              value={value}
-              checked={checked[index]}
-              onChange={() => handleOnChange(index)}
-            />
-            <label className="" htmlFor={`checkbox-${index}`}>
-              {value}
-            </label>
-          </div>
-        </li>
+        return (
+          <li key={index}>
+            <div>
+              <input
+                type="checkbox"
+                name={value}
+                id={`checkbox-${index}`}
+                value={value}
+                checked={checked[index]}
+                onChange={() => handleOnChange(index)}
+              />
+              <label className="ml-2" htmlFor={`checkbox-${index}`}>
+                {value}
+              </label>
+            </div>
+          </li>
+        )
       })}
     </ul>
   )
