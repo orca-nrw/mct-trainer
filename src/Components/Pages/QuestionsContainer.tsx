c1c7ceac0f556022d59368687179ff69a68590f3
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-22 14:14:54 +0100

    added initial content

--- src/Components/Pages/QuestionsContainer.tsx
+++ src/Components/Pages/QuestionsContainer.tsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
+import Question from '../../Types/Question'
+import QuestionHandler from '../UI/QuestionHandler'
+import QuestionSelector from '../UI/QuestionSelector'
+import TrainerHeader from '../UI/TrainerHeader'
 
-export default function QuestionsContainer() {
-  return <div>Questions</div>
+interface Props {
+  selectedQuestions: Question[]
+}
+
+export default function QuestionsContainer({ selectedQuestions }: Props) {
+  // Navigate to category selection if no questions are selected
+  const navigate = useNavigate()
+  if (!selectedQuestions || selectedQuestions.length === 0) {
+    navigate('/quiz/categories')
+  }
+
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+  return (
+    <div className="space-y-4">
+      <TrainerHeader>Laufendes Quiz</TrainerHeader>
+      <QuestionHandler
+        questionList={selectedQuestions}
+        currentQuestion={currentQuestion}
+      />
+      <QuestionSelector
+        numberOfQuestions={selectedQuestions.length}
+        currentQuestion={currentQuestion}
+        setCurrentQuestion={setCurrentQuestion}
+      />
+    </div>
+  )
 }
