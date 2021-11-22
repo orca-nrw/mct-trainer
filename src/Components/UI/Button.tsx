a16c26f24e47f7ce2e9746d0b865c6e30030ab4e
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-22 14:12:25 +0100

    added onclick and disabled

--- src/Components/UI/Button.tsx
+++ src/Components/UI/Button.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 
 interface PropTypes {
   children?: React.ReactNode
+  onClick?: () => void
+  disabled?: boolean
 }
 
-export default function Button({ children }: PropTypes) {
+export default function Button({
+  children,
+  onClick,
+  disabled = false,
+}: PropTypes) {
   return (
     <button
-      className="mt-12 px-4 py-2 bg-th-red hover:bg-red-600 text-white text-lg font-semibold border shadow-md rounded-md cursor-pointer"
+      className={`px-4 py-2 text-white text-lg font-semibold border shadow-md rounded-md cursor-pointer ${
+        disabled
+          ? `bg-gray-400 pointer-events-none`
+          : `bg-th-red hover:bg-red-600`
+      }`}
       type="button"
+      onClick={onClick}
     >
       {children}
     </button>
