386fd6f9ccad82b65d9272f95247ba0ee3d99b63
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-22 17:57:42 +0100

    Added navigation

--- src/Components/Pages/Evaluation.tsx
+++ src/Components/Pages/Evaluation.tsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import Question from '../../Types/Question'
+import TrainerHeader from '../UI/TrainerHeader'
 
-export default function Evaluation() {
-  return <div>Evaluation</div>
+interface Props {
+  selectedQuestions: Question[]
+  selectedAnswers: string[][]
+}
+
+export default function Evaluation({
+  selectedQuestions,
+  selectedAnswers,
+}: Props) {
+  // Navigate to category selection if no questions are selected
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!selectedQuestions || selectedQuestions.length === 0) {
+      navigate('/quiz/categories')
+    }
+  })
+
+  return (
+    <div className="space-y-4">
+      <TrainerHeader>Evaluation</TrainerHeader>
+    </div>
+  )
 }
