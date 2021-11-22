14a29535f65146a4bff54012f5d07b9e47ae8455
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-22 16:58:36 +0100

    Created answer handler for text answers

---
+++ src/Components/UI/TextboxAnswerHandler.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Answer from '../../Types/Answer'
+
+interface Props {
+  answers: Answer[]
+  selectedAnswers: string[]
+  setSelectedAnswers: (selectedAnswers: string[]) => void
+}
+
+export default function TextboxAnswerHandler({
+  answers,
+  selectedAnswers,
+  setSelectedAnswers,
+}: Props) {
+  function handleTextChange(
+    changeEvent: React.FormEvent<HTMLInputElement>,
+    index: number
+  ) {
+    const temp = [...selectedAnswers]
+    temp[index] = changeEvent.currentTarget.value
+    setSelectedAnswers(temp)
+  }
+  return (
+    <div className="space-y-2">
+      {answers.map((answer, index) => {
+        return (
+          <div key={index}>
+            <label className="mr-2" htmlFor={`textfield-${index}`}>
+              {answer.text}
+            </label>
+            <input
+              className="border border-black rounded-md"
+              id={`textfield-${index}`}
+              type="text"
+              value={selectedAnswers[index]}
+              onChange={(changeEvent: React.FormEvent<HTMLInputElement>) =>
+                handleTextChange(changeEvent, index)
+              }
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
