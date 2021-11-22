e1f343896ad39b156f39af2b646328d4ba55cef7
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-11-22 16:59:01 +0100

    Fixed react bug

--- src/Components/Pages/QuestionsContainer.tsx
+++ src/Components/Pages/QuestionsContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
 import Question from '../../Types/Question'
 import QuestionHandler from '../UI/QuestionHandler'
@@ -18,27 +18,35 @@ export default function QuestionsContainer({
 }: Props) {
   // Navigate to category selection if no questions are selected
   const navigate = useNavigate()
-  if (!selectedQuestions || selectedQuestions.length === 0) {
-    navigate('/quiz/categories')
-    return <></>
-  }
+
+  useEffect(() => {
+    if (!selectedQuestions || selectedQuestions.length === 0) {
+      navigate('/quiz/categories')
+    }
+  })
 
   const [currentQuestion, setCurrentQuestion] = useState(0)
 
   return (
     <div className="space-y-4">
       <TrainerHeader>Laufendes Quiz</TrainerHeader>
-      <QuestionHandler
-        questionList={selectedQuestions}
-        currentQuestion={currentQuestion}
-        selectedAnswers={selectedAnswers}
-        setSelectedAnswers={setSelectedAnswers}
-      />
-      <QuestionSelector
-        numberOfQuestions={selectedQuestions.length}
-        currentQuestion={currentQuestion}
-        setCurrentQuestion={setCurrentQuestion}
-      />
+      {!selectedQuestions || selectedQuestions.length === 0 ? (
+        <></>
+      ) : (
+        <>
+          <QuestionHandler
+            questionList={selectedQuestions}
+            currentQuestion={currentQuestion}
+            selectedAnswers={selectedAnswers}
+            setSelectedAnswers={setSelectedAnswers}
+          />
+          <QuestionSelector
+            numberOfQuestions={selectedQuestions.length}
+            currentQuestion={currentQuestion}
+            setCurrentQuestion={setCurrentQuestion}
+          />
+        </>
+      )}
     </div>
   )
 }
