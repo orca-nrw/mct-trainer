af2a6898b024483cdcfffc34303f181f40b7c9b6
Alexander Kosmehl <kosmehl@live.de>
2022-04-09 01:10:19 +0200

    Added fuzzy-logic to handle similar words

--- src/Components/UI/QuestionEvaluation.tsx
+++ src/Components/UI/QuestionEvaluation.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Question from '../../Types/Question'
 import ButtonModal from './ModalButton'
+import levenshtein from 'damerau-levenshtein'
 
 interface Props {
   question: Question
@@ -8,22 +9,44 @@ interface Props {
   index: number
 }
 
+function evaluateQuestion(
+  question: Question,
+  selectedAnswers: string[]
+): 'correct' | 'probably-correct' | 'false' {
+  // Handle Text Questions using the damerau-levenshtein-algorithm to allow very similar strings to pass
+  // Multiple Choice questions can be handled in this way too since the stringified booleans always fall outside the probably correct spectrum
+  const similarities = question.answers.map((answer, index) => {
+    return levenshtein(
+      answer.solution.toLowerCase(),
+      selectedAnswers[index].toLowerCase()
+    ).similarity
+  })
+
+  // Fuzzy logic to handle uncertain answers
+  if (similarities.every((similarity) => similarity === 1.0)) {
+    return 'correct'
+  } else if (similarities.every((similarity) => similarity > 0.85)) {
+    return 'probably-correct'
+  } else return 'false'
+}
+
 export default function QuestionEvaluation({
   question,
   selectedAnswers,
   index,
 }: Props) {
-  const isCorrect = question.answers.every(
-    (answer, index) =>
-      answer.solution.toLowerCase() === selectedAnswers[index].toLowerCase()
-  )
+  const evaluationResult = evaluateQuestion(question, selectedAnswers)
+  const evaluationColor =
+    evaluationResult === 'correct'
+      ? 'bg-green-200 text-green-900'
+      : evaluationResult === 'probably-correct'
+      ? 'bg-yellow-200 text-yellow-900'
+      : 'bg-red-200 text-red-900'
 
   return (
     <div className="border border-gray-300 rounded-md shadow-md">
       <div
-        className={`${
-          isCorrect ? 'bg-green-200 text-green-900' : 'bg-red-200 text-red-900'
-        } px-4 py-2 rounded-t-md border-b border-gray-300 flex justify-between`}
+        className={`${evaluationColor} px-4 py-2 rounded-t-md border-b border-gray-300 flex justify-between`}
       >
         <h1>{index + 1}. Frage</h1>
         <ButtonModal
